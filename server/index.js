ae145015dd4053faa948d8a5d526a738eedc1907
Nikhil-dev13 <nikhil13012000@gmail.com>
2020-12-29 13:39:51 +0530

    remove minor render bug

--- server/index.js
+++ server/index.js
@@ -38,9 +38,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-    const values = await pgClient.query('SELECT * from values');
-
-    res.send(values.rows);
+    try{
+        const values = await pgClient.query('SELECT * from values');
+        res.send(values.rows);
+    } catch (error){
+        console.log(error)
+    }
 });
 
 app.get('/values/current', async (req, res) => {
